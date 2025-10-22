560ddbb1e2b5367fc853af0c6df44d4972a731a4
Fathurrahman Kesuma Ridwan <fathur260605@gmail.com>
2025-10-22 18:41:47 +0700

    spiking API

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import logging
 from dotenv import load_dotenv 
 import os 
+from aiohttp import request
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
@@ -13,9 +14,23 @@ intents.message_content = True
 intents.members = True
 
 bot = commands.Bot(command_prefix='!p', intents=intents)
+URL = os.getenv('URL')
 
 @bot.event
 async def on_ready():
     print("Aman le")
+    print(URL)
+
+@bot.command(name="get")
+async def get_fact(ctx):
+    URL = os.getenv('URL')
+
+    async with request("GET", URL, headers={}) as response: 
+        if response.status == 200:
+            data = await response.json()
+            await ctx.send(data["data"][0])
+        else:
+            await ctx.send(f"Response error {response.status}")
+
 
 bot.run(token, log_handler=handler, log_level=logging.DEBUG)
